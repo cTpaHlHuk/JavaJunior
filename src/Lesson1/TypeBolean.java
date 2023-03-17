da34c018a30cb18194a3becea1fb0ced2cb3a8b0
cTpaHHuk <cTpaHlHuk>
2023-03-17 20:34:30 +0300

    if

---
+++ src/Lesson1/TypeBolean.java
@@ -0,0 +1,21 @@
+package Lesson1;
+
+public class TypeBolean {
+    public static void main(String[] args) {
+        boolean b = true;
+        boolean b1 = false;
+
+        int a = 5;
+        int c = 6;
+
+        boolean q = 5 > 6;
+
+        System.out.println(q);
+
+        if (q == true) {
+            System.out.println("Правда");
+        } else {
+            System.out.println("Это ложь");
+        }
+    }
+}
