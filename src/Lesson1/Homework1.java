d5de9ee6062f58c25af25417d6513b74f37918d8
cTpaHHuk <cTpaHlHuk>
2023-03-17 21:49:33 +0300

    Homework1

---
+++ src/Lesson1/Homework1.java
@@ -0,0 +1,25 @@
+package Lesson1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        //Дано 1000
+        //пицца 135. булка 78, жвачка 3
+        //купить максимум пицц, булок, жвачек на ост
+
+        int pizzaPrice = 135;
+        int bunPrice = 78;
+        int gumPrice = 3;
+        int money = 1000;
+
+
+        int pizzaAmount = money / pizzaPrice;
+        int bunAmount = (money - pizzaAmount * pizzaPrice) / bunPrice;
+        int gumAmount = (money - pizzaAmount * pizzaPrice - bunAmount * bunPrice) / gumPrice;
+        int moneyOst = money - pizzaAmount * pizzaPrice - bunAmount * bunPrice - gumAmount * gumPrice;
+
+        System.out.println("Кол-во пицц = " + pizzaAmount);
+        System.out.println("Кол-во булок = " + bunAmount);
+        System.out.println("Кол-во жвачек = " + gumAmount);
+        System.out.println("Кол-во денег осталось = " + moneyOst);
+    }
+}
