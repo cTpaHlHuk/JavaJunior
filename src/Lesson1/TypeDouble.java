f6e02bd5858aa2e86651840e49c40f7db0d20182
cTpaHHuk <cTpaHlHuk>
2023-03-17 20:23:40 +0300

    дабл

---
+++ src/Lesson1/TypeDouble.java
@@ -0,0 +1,17 @@
+package Lesson1;
+
+public class TypeDouble {
+    public static void main(String[] args) {
+//        float f = 3.14f;
+//        double d = 3.312123123;
+//        System.out.println(d);
+
+        int i = 11;
+        double a = 34;
+
+        double q = a / i;
+
+        System.out.println(q);
+
+    }
+}
