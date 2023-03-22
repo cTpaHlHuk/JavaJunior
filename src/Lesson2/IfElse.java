f5dfd276d2c02c58918627817a20ef97e107fd21
ritaoa <119070741+ritaoa@users.noreply.github.com>
2023-03-22 19:46:47 +0300

    Lesson2

---
+++ src/Lesson2/IfElse.java
@@ -0,0 +1,14 @@
+package Lesson2;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int a = 123123;
+        if (a > 0) {
+            System.out.println("+");
+        } else if (a < 0) {
+            System.out.println("-");
+        } else if (a==0){
+            System.out.println(0);
+        }
+    }
+}
---
+++ src/Lesson2/UpperDown.java
@@ -0,0 +1,10 @@
+package Lesson2;
+
+public class UpperDown {
+    public static void main(String[] args) {
+        int qwe = 123;
+        double d = qwe;
+        double asd = 23423423;
+        int i = (int) asd;
+    }
+}
