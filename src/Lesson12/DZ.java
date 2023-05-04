2d338b3959c6e6ea123ae48efc8cf59f610f0cd7
cTpaHHuk <cTpaHlHuk>
2023-05-04 22:24:33 +0300

    дз 12ого урока

--- src/Lesson12/DZ.java
+++ src/Lesson12/DZ.java
@@ -1,5 +1,8 @@
 package Lesson12;
 
+import java.util.ArrayList;
+
+
 public class DZ {
     public static void main(String[] args) {
 
@@ -9,5 +12,32 @@ public class DZ {
         //симетрическая разность 1245
         //объединение 123345
         //разность 12   делать через методы
+
+        ArrayList<Integer> arrayListA = new ArrayList<>();
+        for (int i = 1; i < 4; i++) {
+            arrayListA.add(i);
+        }
+        ArrayList<Integer> arrayListB = new ArrayList<>();
+        for (int i = 3; i < 6; i++) {
+            arrayListB.add(i);
+        }
+
+        ArrayList<Integer> arrayListC = new ArrayList<>();
+        arrayListC.addAll(arrayListA);
+        arrayListC.addAll(arrayListB);
+
+        System.out.println("Объединение коллекций " + arrayListC);
+
+        arrayListC.retainAll(arrayListA);
+        arrayListC.retainAll(arrayListB);
+
+        System.out.println("Пересечение коллекции " + arrayListC);
+
+        arrayListA.removeAll(arrayListB);
+
+        System.out.println("Ассиметричная разность " + arrayListA);
+
+
+
     }
 }
