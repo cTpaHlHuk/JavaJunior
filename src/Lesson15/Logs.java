702e41642718aeda7ff50d9319556f9d1bb106f5
cTpaHHuk <cTpaHlHuk>
2023-05-15 14:39:00 +0300

    log4j

--- src/Lesson15/Logs.java
+++ src/Lesson15/Logs.java
@@ -6,6 +6,6 @@ public class Logs {
     public static final Logger LOGGER = Logger.getLogger(Logs.class);
     public static void main(String[] args) {
         System.out.println();
-        LOGGER.error("Error message");
+        LOGGER.error("Error message  ");
     }
 }
